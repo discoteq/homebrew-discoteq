de704d6bac829715a9528f6635052bd575148d55
Joseph Anthony Pasquale Holsten <joseph@josephholsten.com>
2014-08-18 23:36:13 +0000

    flock.rb, README.md: add

---
+++ README.md
@@ -0,0 +1,23 @@
+Homebrew-discoteq
+==============
+
+These formulae provide software created by the discoteq project, including:
+
+* [`flock`](https://github.com/discoteq/flock): locks files from the command line
+
+
+How do I install these formulae?
+--------------------------------
+Just `brew tap discoteq/discoteq` and then `brew install <formula>`.
+
+You can also install via URL:
+
+
+    brew install https://raw.githubusercontent.com/discoteq/homebrew-discoteq/master/<formula>.rb
+
+Docs
+----
+`brew help`, `man brew`, or the Homebrew [wiki][].
+
+[wiki]:http://wiki.github.com/Homebrew/homebrew
+
---
+++ flock.rb
@@ -0,0 +1,39 @@
+require "formula"
+
+# Documentation: https://github.com/Homebrew/homebrew/wiki/Formula-Cookbook
+#                /usr/local/Library/Contributions/example-formula.rb
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class Flock < Formula
+  homepage "https://github.com/discoteq/flock"
+  url "https://github.com/discoteq/flock/releases/download/v0.1.1/flock-0.1.1.tar.xz"
+  sha1 "0ceecb94d881af601e90b3dfb2ed1e1be9a0719d"
+
+  # depends_on "cmake" => :build
+  # depends_on :x11 # if your formula requires any X11/XQuartz components
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+
+    # Remove unrecognized options if warned by configure
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+    system "make", "install" # if this fails, try separate make/make install steps
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! It's enough to just replace
+    # "false" with the main program this formula installs, but it'd be nice if you
+    # were more thorough. Run the test with `brew test flock`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "true"
+  end
+end
